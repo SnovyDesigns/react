24695a40d3f17866a394180c4fe277e900c026d3
Krzysztof Piekarz <kpiekarz1983@gmail.com>
2018-06-11 12:36:50 +0200

    Finished 14.7

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -6,17 +6,59 @@ import createReactClass from 'create-react-class';
 import Search from './Search.jsx';
 import Gif from './Gif.jsx';
 
+const GIPHY_API_URL = 'https://api.giphy.com';
+const GIPHY_PUB_KEY = 'Pdp1hN2pop8rKyGkMTV242QC9AcJnnfa';
+
 
 const App = createReactClass({
+    getInitialState() {
+        return {
+            loading: false,
+            searchingText: '',
+            gif: {}
+        };
+    },
     render: function() {
         return (
             <div className="app">
                 <h1>Wyszukiwarka GIFow!</h1>
                 <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj Enter, aby pobrać kolejne gify.</p>
-                <Search />
-                <Gif />
+                <Search onSearch={this.handleSearch}/>
+                <Gif
+                    loading={this.state.loading}
+                    url={this.state.gif.url}
+                    sourceUrl={this.state.gif.sourceUrl}
+                />
             </div>
         );
+    },
+    handleSearch: function(searchingText) { 
+        this.setState({
+            loading: true  
+        });
+        this.getGif(searchingText, function(gif) {  
+            this.setState({  
+                loading: false,  
+                gif: gif,  
+                searchingText: searchingText  
+            });
+        }.bind(this));
+    },
+    getGif: function(searchingText, callback) { 
+        let url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  
+        let xhr = new XMLHttpRequest();  
+        xhr.open('GET', url);
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                let data = JSON.parse(xhr.responseText).data; 
+                let gif = {  
+                    url: data.fixed_width_downsampled_url,
+                    sourceUrl: data.url
+                };
+                callback(gif);  
+            }
+        };
+        xhr.send();
     }
 });
 
--- src/app/components/Search.jsx
+++ src/app/components/Search.jsx
@@ -12,14 +12,29 @@ const Search = createReactClass({
     },
     render: function() {
         return (
-            <input type="text" className="search" placeholder="Tutaj wpisz wyszukiwaną frazę" value={this.getInitialState.searchingText} onChange={this.handleChange}/>
+            <input 
+                type="text" 
+                className="search" 
+                placeholder="Tutaj wpisz wyszukiwaną frazę" 
+                value={this.state.searchingText} 
+                onChange={this.handleChange} 
+                onKeyUp={this.handleKeyUp}
+            />
         );
     },
     handleChange: function(event) {
-        let searchingText = event.targe.value;
+        let searchingText = event.target.value;
         this.setState({
             searchingText: searchingText
         });
+        if (searchingText.length > 2) {
+            this.props.onSearch(searchingText);
+        }
+    },
+    handleKeyUp: function(event) {
+        if (event.keyCode === 13) {
+            this.props.onSearch(this.state.searchingText);
+        }
     }
 });
 
