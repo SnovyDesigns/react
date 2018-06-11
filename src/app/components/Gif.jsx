1d5e48989bb3aa9045ac89438f7891015b8a3a8f
Krzysztof Piekarz <kpiekarz1983@gmail.com>
2018-06-11 12:00:50 +0200

    Add Gif component

--- src/app/components/Gif.jsx
+++ src/app/components/Gif.jsx
@@ -1,62 +1,24 @@
+import '../../sass/Gif.scss';
+
 import React from 'react';
 import createReactClass from 'create-react-class';
 
-const Counter = createReactClass({
-    getDefaultProps: function() {
-        console.log('getDefaultProps');
-    },
-    getInitialState:function() {
-        console.log('getInitialState');
-        return {
-            counter: 0
-        };
-    },
-    componentWillMount: function() {
-        console.log('componentWillMount');
-    },
-    increment: function() {
-        this.setState({
-            counter: this.state.counter + 1
-        });
-    },
-    decrement: function() {
-        let updatedCounter = this.state.counter;
-        updatedCounter === 0 ? 0 : updatedCounter--;
-        this.setState({
-            counter: updatedCounter
-        });
+const GIPHY_LOADING_URL = 'http://www.ifmo.ru/images/loader.gif';
+
+const Gif = createReactClass({
+    getUrl: function() {
+        return this.props.sourceUrl || GIPHY_LOADING_URL;
     },
     render: function() {
+        let url = this.props.loading ? GIPHY_LOADING_URL : this.props.url;
         return (
-            <div className="container" >
-                <div className="row justify-content-center">
-                    <div className="card col-4 text-center mt-4">
-                        <h6 className="card-subtitle mb-2 text-muted mt-3">Stan licznika:</h6>
-                        <h5 className="card-title">{this.state.counter}</h5>
-                        <div className="row justify-content-around mt-3 mb-3">
-                            <button className="btn btn-outline-success" onClick={this.increment}>Zwiększ licznik</button>
-                            <button className="btn btn-outline-danger" onClick={this.decrement}>Zmniejsz licznik</button>
-                        </div>
-                    </div>
-                </div>
+            <div className="gif-container">
+                <a href={this.getUrl()} title='view this on giphy' target='new'>
+                    <img id='gif' src={url} />
+                </a>
             </div>
-        );        
-    },
-    componentDidMount: function() {
-        console.log('componentDidMount');
-    },
-    componentWillReceiveProps: function() {
-        console.log('componentWillReceiveProps');
-    },
-    componentWillUpdate: function() {
-        console.log('componentWillUpdate');
-    },
-    componentDidUpdate: function() {
-        console.log('componentDidUpdate');
-    },
-    componentWillUnmount: function() {
-        console.log('componentWillUnmount');
+        );
     }
 });
 
-export default Counter;
+export default Gif;
